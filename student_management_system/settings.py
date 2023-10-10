ee35696da5a38547c39e9e0e0e6469300e7f8b65
praiseolotu <olioxxayo@gmail.com>
2023-10-10 20:20:38 +0100

    db setup

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -73,14 +73,14 @@ WSGI_APPLICATION = 'student_management_system.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
