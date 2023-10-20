83455ef6a2008fc36113297c14d4507edfa203c8
praiseolotu <olioxxayo@gmail.com>
2023-10-20 21:41:27 +0100

    edited urlconfig

--- student_management_system/urls.py
+++ student_management_system/urls.py
@@ -23,5 +23,5 @@ from student_management_system import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('student_management_app.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URLS, document_root=settings.MEDIA_ROOT)
 #urlpatterns += staticfiles_urlpatterns()
